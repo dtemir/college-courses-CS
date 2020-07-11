1da70eddd3bf9f714fd2d6e8599d8e2dd0ed8b39
Damir Temir <temirdamir10@gmail.com>
2020-07-11 16:15:21 +0100

    HW - 1
    Features: Scanner class; Math class (Math.pow), System class, Double data type.
    conversion.java converts pounds into killograms by reading a value into the double data type and multiplying it by a constant of 0.454.
    distance.java calculates the distance between two points on the two-dimensional axis by using Math.pow.

---
+++ CSC 225/conversion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Conversion {
+	
+	public static void main(String[] args) {
+		
+		// Scanner object
+		Scanner input = new Scanner (System.in);
+		// Prompt to enter a number
+		System.out.print("Enter a number in pounds: ");
+		// Assigning the number
+		double pounds = input.nextDouble();
+		// Calculating and assigning the result
+		double killograms = pounds * 0.454;
+		// Output
+		System.out.println(pounds + " pounds is " + killograms + " killograms.");
+		
+	}
+
+}
