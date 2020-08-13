afa38de58b427ab6d2c36ec51fa0ac8b0f6b9f9c
dtemir <dtemi2@uis.edu>
2020-08-12 20:08:51 -0500

    exercise-1-1.py

---
+++ CSC 302/Exercise #1/exercise-1-1.py
@@ -0,0 +1,14 @@
+count = 0
+total = 0
+average = 0
+
+print("Enter grades one a time, enter -1 to finish")
+i = int(input())
+while i != -1:
+    count += 1
+    total += i
+    i = int(input())
+
+average = total / count
+
+print(average)
---
+++ CSC 302/Exercise #1/exercise-1-2.py
@@ -0,0 +1,11 @@
+count = 25
+total = 0
+average = 0
+
+for j in range(count):
+    i = int(input())
+    total += i
+
+average = total / count
+
+print(average)
