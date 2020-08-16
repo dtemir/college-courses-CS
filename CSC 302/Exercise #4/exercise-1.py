4469346b485d2a7afbd4894484b511b9614d7e78
dtemir <dtemi2@uis.edu>
2020-08-16 11:43:30 -0500

    exercise-1.py

---
+++ CSC 302/Exercise #4/exercise-1.py
@@ -0,0 +1,16 @@
+"""This is the implementation of the Exercise 4"""
+string_list = list(input())  # Convert user input to a list
+
+string_output = []  # Create a list for storing the reversed list
+length = len(string_list)  # Assign a variable to represent the length of the string
+
+for i in range(length):  # Iterate through the string list
+    # Append values to the output string by taking last elements from
+    # the string list
+    string_output.append(string_list[-(i + 1)])
+
+print(string_output)
+
+# Pythonic version
+string_list.reverse()
+print(string_list)
