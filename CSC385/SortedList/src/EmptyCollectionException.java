f37bf7be3088608f94abda82599d6e4d24f8983d
Damir <dtemi2@uis.edu>
2021-03-13 20:52:20 -0600

    Initialized Sorted List Assignment (CSC385)

---
+++ CSC385/SortedList/src/EmptyCollectionException.java
@@ -0,0 +1,13 @@
+
+public class EmptyCollectionException extends RuntimeException {
+
+	public EmptyCollectionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public EmptyCollectionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ CSC385/SortedList/src/SortedList.java
@@ -0,0 +1,61 @@
+
+public class SortedList<T extends Comparable<? super T>> {
+	
+	//Fields
+	
+	//Constructor
+	public SortedList() {
+		clear();
+	}
+	
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public int getSize() {
+		return 0;
+	}
+	
+	public void add(T data) {
+		
+	}
+	
+	private void append(T data) {
+		
+	}
+	
+	public T removeAt(int index) {
+		return null;
+	}
+	
+	public T get(int index) {
+		return null;
+	}
+	
+	public boolean contains(T data) {
+		return false;
+	}
+	
+	public void removeAll(SortedList<T> otherList) {
+		
+	}
+	
+	private void removeNode(Node node) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+	}
+	
+	//Your node class.  You may move it to a different file but
+	//you will need to change this to public class Node
+	private class Node {
+		
+	}
+}
