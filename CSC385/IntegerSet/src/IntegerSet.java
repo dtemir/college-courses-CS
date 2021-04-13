a9e0fe2b99bb23ad95776655fc57a06d7a9f9638
Damir <dtemi2@uis.edu>
2021-04-12 19:43:41 -0500

    Initialized IntegerSet (CSC385)

---
+++ CSC385/IntegerSet/src/IntegerSet.java
@@ -0,0 +1,36 @@
+public class IntegerSet {
+
+    private int size;
+    
+    public IntegerSet() {
+	//Empty set
+    }
+    
+    public IntegerSet(int array[]) {
+	//Add elements of array to Binary Search Tree
+    }
+
+    public boolean add(int newItem) {
+        return false;
+    }
+
+    public boolean contains(int itemToFind) {
+        return false;
+    }
+
+    public IntegerSet union(IntegerSet other) {
+        return null;
+    }
+
+    public IntegerSet intersection(IntegerSet other) {
+        return null;
+    }
+
+    public int magnitude() {
+        return size;
+    }
+
+    public String toString() {
+        return null;
+    }
+}
